aa303a28e8fa7df212c67ce510c4ae41977acf82
Jolman Gonzalez <jolmangonzalez5@gmail.com>
2024-01-23 19:28:54 -0500

    primer comit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True,load_dotenv=True)
